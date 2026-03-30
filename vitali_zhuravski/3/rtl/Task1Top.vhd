a4cb7a5ba5a224e99fd2ab20cb8b601d39a80af7
Vitali Zhuravski <vitaliycof@outlook.com>
2026-03-31 00:23:18 +0300

    Инициализировал новый проект.

---
+++ vitali_zhuravski/3/rtl/Task1Top.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Task1Top is
+--  Port ( );
+end Task1Top;
+
+architecture Behavioral of Task1Top is
+
+begin
+
+
+end Behavioral;
